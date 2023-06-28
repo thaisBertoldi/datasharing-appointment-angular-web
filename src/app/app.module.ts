dbbd60edad9de4b4b86c11bcca07d72918196195
thaisBertoldi <thais_brt@hotmail.com>
2023-06-28 19:43:45 -0300

    feat: created component navbar and adjusted routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './view/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './view/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './view/templates/navbar/navbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
